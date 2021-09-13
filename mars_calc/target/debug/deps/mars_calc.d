36dbf972e32d1e52a2b68da74af4f8884cd5bc81
Len <len.choi@cyworldlabs.com>
2021-09-13 23:55:57 +0900

    first commit

---
+++ mars_calc/target/debug/deps/mars_calc.d
@@ -0,0 +1,5 @@
+/Users/labs/Documents/dev/example/mars_calc/target/debug/deps/mars_calc: src/main.rs
+
+/Users/labs/Documents/dev/example/mars_calc/target/debug/deps/mars_calc.d: src/main.rs
+
+src/main.rs:
---
+++ mars_calc/target/debug/mars_calc.d
@@ -0,0 +1,1 @@
+/Users/labs/Documents/dev/example/mars_calc/target/debug/mars_calc: /Users/labs/Documents/dev/example/mars_calc/src/main.rs
